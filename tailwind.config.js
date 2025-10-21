d980793bc78c05524aab22462037fcee91951b87
PalmCodeTech <palmcodetech@gmail.com>
2025-10-21 22:36:35 +0000

    Testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}", "./src/**/*.{html,js}"],
+  darkMode: 'class', // Enables dark mode via a class
+  content: [
+    "./index.html",           // Include your main HTML
+    "./*.{html,js}",          // Include any HTML/JS files in root
+    "./src/**/*.{html,js}"    // Include all files inside src folder
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
